cbe6e76a2d123c9ef486739ed5719ab63e1c9746
Ondrej Vostal <ondrej@vostal.net>
2022-06-04 09:35:24 +0200

    [20220604_workshop_dex_arb] Add part-b scaffold

---
+++ 20220604_workshop_dex_arb/part-b/config.js
@@ -0,0 +1,7 @@
+const alchemy_key = 'cQ7eqtboeO428yWiVB-Qu_4gdBF-Wp-J'
+const alchemy_endpoint = 'https://eth-mainnet.alchemyapi.io/v2/' + alchemy_key;
+
+module.exports = {
+  alchemy_key,
+  alchemy_endpoint,
+};
